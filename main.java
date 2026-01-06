61016af8c35a1096a6fa5abece92eb1aad01e5be
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-06 22:48:01 +0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+package javadev;
+import javadev.student;
+public class main {
+
+    public static void main(String[] args) {
+        student s1 = new student("Fatima", 20, "12345");
+        s1.displayInfo();
+    }
+
+}
